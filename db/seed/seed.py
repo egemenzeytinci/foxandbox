9f7d773ba00595fdf350b07ef50db2cf70e19a78
egemenzeytinci <egemenzeytinci@gmail.com>
2021-09-06 13:55:39 +0300

    Add new title types to non-allowed

--- db/seed/seed.py
+++ db/seed/seed.py
@@ -33,6 +33,8 @@ class Seed:
             'audiobook',
             'radioSeries',
             'episode',
+            'radioEpisode',
+            'tvPilot',
         ]
 
         # non-boolean values
