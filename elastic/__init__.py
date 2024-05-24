0fd9b3bdd939603bc779acf15cf10476357f6e39
egemenzeytinci <egemenzeytinci@gmail.com>
2024-05-25 00:10:53 +0300

    Update elastic connection

--- elastic/__init__.py
+++ elastic/__init__.py
@@ -1,8 +1,16 @@
 from elasticsearch import Elasticsearch
 from util.config import config
 
+from elasticsearch import Elasticsearch
+from util.config import config
+
 es = Elasticsearch(
     hosts=config.elastic.hosts,
     port=config.elastic.port,
+    http_auth=(
+        config.elastic.username,
+        config.elastic.password
+    ),
     retry_on_timeout=True
 )
+
